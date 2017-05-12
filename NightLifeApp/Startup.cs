19c657a0067d01bace3dd342c3e924dbc4b31c3f
Matthew McDade <Matthew.McDade13@gmail.com>
2017-05-12 02:15:42 -0700

    Added Facebook Authentication and set up User Model in Database

--- NightLifeApp/Startup.cs
+++ NightLifeApp/Startup.cs
@@ -7,16 +7,55 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
+using NightLifeApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace NightLifeApp
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+
+        }
+
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddSingleton(Configuration);
+
+            services.AddDbContext<NightLifeContext>();
+
+            services.AddIdentity<NightLifeUser, IdentityRole>(options =>
+            {
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+-/=?^_`{|}~.@ ";
+            })
+                .AddEntityFrameworkStores<NightLifeContext>();
+
+            services.AddMvc()
+                .AddJsonOptions( options => {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,9 +65,30 @@ namespace NightLifeApp
 
             app.UseStaticFiles();
 
+            app.UseIdentity();
+
+            app.UseFacebookAuthentication(new FacebookOptions()
+            {
+                AppId = Configuration["Authentication:Facebook:AppId"],
+                AppSecret = Configuration["Authentication:Facebook:AppSecret"],
+                AutomaticChallenge = true,
+                AutomaticAuthenticate = true,
+                Scope = { "public_profile", "email" }
+
+            });
+
+            //Havent got the Keys for Twitter just yet
+
+            //app.UseTwitterAuthentication(new TwitterOptions()
+            //{
+            //    ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"],
+            //    ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"]
+            //});
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
             }            
 
             app.UseMvc(config =>
